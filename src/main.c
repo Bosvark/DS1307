75db42bf3fa34280addd1673e5ffa081b7ea1651
Dieter Oelofse <dietero2@verifone.com>
2013-12-30 14:59:57 +0200

    Added printing of version and usage info

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+#define APP_NAME	"DS1307"
+
+#define VERSION_MAJOR	0
+#define VERSION_MINOR	1
+
+void print_usage(void)
+{
+	printf("%s %d.%d\n", APP_NAME, VERSION_MAJOR, VERSION_MINOR);
+}
+
 int main(int argc, const char* argv[])
 {
+	if(argc == 1){
+		print_usage();
+		return 0;
+	}
 	return 0;
 }
